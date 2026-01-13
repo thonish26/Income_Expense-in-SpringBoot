849201f57af2bcc57804c969c2a0818f1f85f971
Thonish M <thonishm26@gmail.com>
2026-01-13 15:25:41 +0530

    Add files via upload

---
+++ Income_and_expense-springboot/src/test/java/com/example/Income_and_expense/springboot/IncomeAndExpenseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Income_and_expense.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeAndExpenseSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
